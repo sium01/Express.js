f90ef886e77fcbcce1238f1e47d45eebe78903b0
sium01 <156007391+sium01@users.noreply.github.com>
2025-02-01 08:40:31 +0600

    Create in1.js

---
+++ practice1/in1.js
@@ -0,0 +1,18 @@
+var ex=require("express");
+ap=ex();
+ap.get("/",function(req,res){
+res.send("This is Home Page!");
+});
+ap.get("/blogs",function(req,res){
+res.send("This is blog page!")
+
+});
+ap.get("/about",function(req,res){
+ res.send("This is about page!");
+});
+ap.get("/contact",function(req,res){
+ res.send("This is Contact Page of the Software enginner!")
+})
+ap.listen(3030,function(){
+console.log("Hello Babe!");
+})
